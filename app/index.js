73604634c779ba7fcf92533a0839737ce595761a
jennPeavler <jenn.peavler@gmail.com>
2017-05-16 14:51:14 -0600

    Revert "Jp initial setup"

--- app/index.js
+++ app/index.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import AppContainer from './components/App/AppContainer';
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import App from './components/app';
+
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import rootReducer from './reducers'
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Route, Link, Switch} from 'react-router-dom'
-import promiseMiddleware from 'redux-promise'
-import createHistory from 'history/createBrowserHistory'
-import thunk from 'redux-thunk'
 
 
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(rootReducer, devTools, applyMiddleware(...middleware))
-const middleware = [routerMiddleware(history), thunk]
-const history = createHistory();
+
+const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(<Provider store={store}>
-                  <ConnectedRouter history={history}>
-                    <Route to='/' component={AppContainer}/>
-                  </ConnectedRouter>
+                  <Router >
+                    <Route to='/' component={App}/>
+                  </Router>
                 </Provider>, document.getElementById('main'))
-
-
-// <Router history={history}>
