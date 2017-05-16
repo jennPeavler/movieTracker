448f4ebbe0243a64d4433e5b372c29237f9ff2fd
Chris Jordan <cjorda15@msudenver.edu>
2017-05-16 12:16:54 -0600

    somewhat working api call?

---
+++ app/actions/getMovies.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=4cdebcbe2bc4761f0c631321a04c6465&language=en-US&page=1`
+
+  function fetchMovieRequest(){
+   return axios.get(url)
+}
+
+
+ function fetchMovie(stu){
+   apiCall().then((response)=>{
+    return {
+      type: "FETCH_REQUEST",
+      payload:response
+    }
+  })
+
+}
+
+//  function fetchPostsSuccess(payload) {
+//   return {
+//     type: "FETCH_SUCCESS",
+//     payload
+//   }
+// }
+//
+//  function fetchPostsError() {
+//   return {
+//     type: "FETCH_ERROR"
+//   }
+// }
+
+// const fetchMovieRequest = () => {
+//   return dispatch => {
+//     fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=ccabf0973ffb8f3adedb24eb276dc907')
+//     .then((response) => {
+//       return response.json()
+//     })
+//     .then((films) => {
+//     return  dispatch(getFilms(films.results))
+//     })
+//   }
+// }
+//
+// export const getFilms = films => {
+//   return {
+//     type: 'GET_FILMS',
+//     films
+//   }
+// }
--- app/index.js
+++ app/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Route, Link, Switch} from 'react-router-dom'
-
+import promiseMiddleware from 'redux-promise';
 
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(rootReducer, devTools);
+const store = createStore(rootReducer, devTools,applyMiddleware(promiseMiddleware));
 
 ReactDOM.render(<Provider store={store}>
                   <Router >
