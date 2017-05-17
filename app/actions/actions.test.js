fa6118f5ae56407165d95cac2dbdefe343014d59
John Binning <john.binning17@gmail.com>
2017-05-16 23:23:15 -0600

    add reducer and action tests

---
+++ app/actions/actions.test.js
@@ -0,0 +1,25 @@
+import { toggleMovie, fetchMovieRequest } from './index.js'
+
+describe('actions tests', () => {
+
+  it('should return a toggle a favorite', () => {
+    const id = 42
+    const expectedAction = {
+      type: 'TOGGLE_FAVORITE',
+      id : 42
+    }
+    expect(toggleMovie(id)).toEqual(expectedAction)
+  })
+
+  it('should return a payload', () => {
+    const response = {
+      page: 1,
+      results: [1, 2, 'hello there']
+    }
+    const expectedAction = {
+      type: 'FETCH_REQUEST',
+      payload: response
+    }
+    expect(fetchMovieRequest(response)).toEqual(expectedAction)
+  })
+})
--- app/actions/index.js
+++ app/actions/index.js
@@ -13,5 +13,3 @@ export function toggleMovie(id){
       payload:response
     }
   }
-
-  // function
