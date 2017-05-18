1fdda3539045cc5e690724a8d9ca562e098c8bd6
Chris Jordan <cjorda15@msudenver.edu>
2017-05-18 10:42:44 -0600

    added css for moviecards when favorited/unfavorited

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const cors = require('express-cors');
 var bodyParser = require('body-parser')
-const port = (process.env.PORT || 7700);
+const port = (process.env.PORT || 3300);
 const app = express();
 const users = require('./routes/usersApi');
 
