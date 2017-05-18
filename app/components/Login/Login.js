6a5bcd04cb3430240fd5fd7e5ee689938a479fe7
Chris Jordan <cjorda15@msudenver.edu>
2017-05-18 14:36:57 -0600

    user list from local api now in synch with redux state, doesn t add a class of favorite to cards that should be tho

--- app/components/Login/Login.js
+++ app/components/Login/Login.js
@@ -22,6 +22,15 @@ class Login extends Component {
               if ( user.email === userName.toLowerCase() ){
                 signedOn = true
                 this.props.handleUser({id:user.id})
+
+                fetch(`http://localhost:5000/api//users/${user.id}/favorites`)
+                 .then( response => response.json()).then((res)=>{
+                   res.data.forEach(movie=>{
+                     this.props.handleAddFavorite(movie)
+                   })
+                 })
+
+
               }
           })
           if( signedOn === false ) {
