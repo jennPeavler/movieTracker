7be33aebfe4de4e6e7ca944f840ccb83392b87af
Chris Jordan <cjorda15@msudenver.edu>
2017-05-19 16:11:33 -0600

    about to attempt favorite button on moviedetails component

--- app/components/MovieDetails/MovieDetails.js
+++ app/components/MovieDetails/MovieDetails.js
@@ -5,12 +5,15 @@ const MovieDetails = ({title,overview, vote_average,poster_path,popularity,relea
   return (
     <div className="movie-details">
       <h3 className="movie-details-title">{title}</h3>
-      <img className="movie-details-img" src={`https://image.tmdb.org/t/p/original${poster_path}`}/>
+      <img
+       className="movie-details-img" src={`https://image.tmdb.org/t/p/original${poster_path}`}/>
       <p className="movie-details-overview">{overview}</p>
-      <p className="movie-details-release">release date: {release_date}</p>
-      <p className="movie-details-popularity">popularity:{popularity}</p>
-      <p className="movie-details-vote-average">vote average:{vote_average}</p>
-      <p className="movie-details-vote-average">vote count:{vote_count}</p>
+      <div className="movie-details-info-container">
+        <p className="movie-details-release"><span className="movie-details-category">release date:</span> {release_date}</p>
+        <p className="movie-details-popularity"><span className="movie-details-category">popularity:</span>{popularity}</p>
+        <p className="movie-details-vote-average"><span className="movie-details-category">vote average:</span>{vote_average}</p>
+        <p className="movie-details-vote-average"><span className="movie-details-category">vote count:</span>{vote_count}</p>
+      </div>
     </div>
   )
 }
