911eee3a3f0388950095f9f12030dc23b3d047a0
Chris Jordan <cjorda15@msudenver.edu>
2017-05-20 10:23:09 -0600

    favorties test completed?

--- app/components/Favorites/Favorites.test.js
+++ app/components/Favorites/Favorites.test.js
@@ -1,16 +1,50 @@
-// import React from 'react'
-// import Favorites from './Favorites'
-// import { shallow, mount } from 'enzyme'
-
-import React, { Component } from 'react'
+import Favorites from './Favorites'
 import { shallow, mount } from 'enzyme'
+import MovieCard from '../MovieCard/MovieCard'
+import React, { Component } from 'react'
 
-import Login from '../Login/Login'
 
-describe('Login functionality', () => {
+describe('Favorite functionality', () => {
 
-it('should have a placeholder', () => {
-  const wrapper = shallow(Favorites)
+it('should render', () => {
+  const wrapper = shallow(
+    <Favorites
+      movies={[]}
+      favorites={[]}
+      user={12}/>)
 
+expect(wrapper.length).toEqual(1)
   })
+
+  it("should render text if no favortite movies are found",()=>{
+
+    const wrapper = shallow(
+      <Favorites
+        movies={[]}
+        favorites={[]}
+        user={12}/>)
+  expect(wrapper.find('.no-favorites-container').text()).toEqual("No favorite movies? Come on...")
+  expect(wrapper.children().length).toEqual(1)
+  })
+
+  it("should render a list of favorites if any ",()=>{
+
+    const wrapper = shallow(
+      <Favorites
+        movies={[{id:12},{id:15},{id:17}]}
+        favorites={[12,15]}
+        user={2}/>)
+    expect(wrapper.find('MovieCard').length).toEqual(2)
+  })
+
+  it("should have a class name if favorites are available",()=>{
+
+    const wrapper = shallow(
+      <Favorites
+        movies={[{id:12},{id:15},{id:17}]}
+        favorites={[12,15]}
+        user={2}/>)
+        expect(wrapper.node.props.className).toEqual('favorites-list')
+  })
+
 })
