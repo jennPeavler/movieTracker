fd3d132150a6bfedfb3993a2eaab8ee3ffb39add
Jennifer Peavler <jenn.peavler@gmail.com>
2017-05-19 21:40:00 -0600

    add movieList tests and add jest object to package.json in order to not parse css, also npm install --save-dev identity-obj-proxy

---
+++ app/components/MovieList/movieData.js
@@ -0,0 +1,409 @@
+module.exports =
+
+{
+  "page": 1,
+  "movies": [
+    {
+      "poster_path": "/y4MBh0EjBlMuOzv9axM4qJlmhzz.jpg",
+      "adult": false,
+      "overview": "The Guardians must fight to keep their newfound family together as they unravel the mysteries of Peter Quill's true parentage.",
+      "release_date": "2017-04-24",
+      "genre_ids": [
+        12,
+        28,
+        35,
+        878
+      ],
+      "id": 283995,
+      "original_title": "Guardians of the Galaxy Vol. 2",
+      "original_language": "en",
+      "title": "Guardians of the Galaxy Vol. 2",
+      "backdrop_path": "/aJn9XeesqsrSLKcHfHP4u5985hn.jpg",
+      "popularity": 113.145058,
+      "vote_count": 1575,
+      "video": false,
+      "vote_average": 7.6
+    },
+    {
+      "poster_path": "/ewVHnq4lUiovxBCu64qxq5bT2lu.jpg",
+      "adult": false,
+      "overview": "Bound for a remote planet on the far side of the galaxy, the crew of the colony ship Covenant discovers what they think is an uncharted paradise, but is actually a dark, dangerous world — whose sole inhabitant is the “synthetic” David, survivor of the doomed Prometheus expedition.",
+      "release_date": "2017-05-09",
+      "genre_ids": [
+        53,
+        27,
+        878
+      ],
+      "id": 126889,
+      "original_title": "Alien: Covenant",
+      "original_language": "en",
+      "title": "Alien: Covenant",
+      "backdrop_path": "/kMU8trT43p5LFoJ4plIySMOsZ1T.jpg",
+      "popularity": 67.498386,
+      "vote_count": 353,
+      "video": false,
+      "vote_average": 5.9
+    },
+    {
+      "poster_path": "/iNpz2DgTsTMPaDRZq2tnbqjL2vF.jpg",
+      "adult": false,
+      "overview": "When a mysterious woman seduces Dom into the world of crime and a betrayal of those closest to him, the crew face trials that will test them as never before.",
+      "release_date": "2017-04-12",
+      "genre_ids": [
+        28,
+        80,
+        53
+      ],
+      "id": 337339,
+      "original_title": "The Fate of the Furious",
+      "original_language": "en",
+      "title": "The Fate of the Furious",
+      "backdrop_path": "/jzdnhRhG0dsuYorwvSqPqqnM1cV.jpg",
+      "popularity": 62.624597,
+      "vote_count": 1842,
+      "video": false,
+      "vote_average": 6.7
+    },
+    {
+      "poster_path": "/1SwAVYpuLj8KsHxllTF8Dt9dSSX.jpg",
+      "adult": false,
+      "overview": "Chris and his girlfriend Rose go upstate to visit her parents for the weekend. At first, Chris reads the family's overly accommodating behavior as nervous attempts to deal with their daughter's interracial relationship, but as the weekend progresses, a series of increasingly disturbing discoveries lead him to a truth that he never could have imagined.",
+      "release_date": "2017-02-24",
+      "genre_ids": [
+        9648,
+        53,
+        27
+      ],
+      "id": 419430,
+      "original_title": "Get Out",
+      "original_language": "en",
+      "title": "Get Out",
+      "backdrop_path": "/Ae58bf7Yj6OPzwKerPgXSnxCJdh.jpg",
+      "popularity": 32.959904,
+      "vote_count": 1061,
+      "video": false,
+      "vote_average": 7.1
+    },
+    {
+      "poster_path": "/rXMWOZiCt6eMX22jWuTOSdQ98bY.jpg",
+      "adult": false,
+      "overview": "Though Kevin has evidenced 23 personalities to his trusted psychiatrist, Dr. Fletcher, there remains one still submerged who is set to materialize and dominate all the others. Compelled to abduct three teenage girls led by the willful, observant Casey, Kevin reaches a war for survival among all of those contained within him — as well as everyone around him — as the walls between his compartments shatter apart.",
+      "release_date": "2016-11-15",
+      "genre_ids": [
+        27,
+        53
+      ],
+      "id": 381288,
+      "original_title": "Split",
+      "original_language": "en",
+      "title": "Split",
+      "backdrop_path": "/4G6FNNLSIVrwSRZyFs91hQ3lZtD.jpg",
+      "popularity": 32.877683,
+      "vote_count": 2520,
+      "video": false,
+      "vote_average": 6.8
+    },
+    {
+      "poster_path": "/qwoGfcg6YUS55nUweKGujHE54Wy.jpg",
+      "adult": false,
+      "overview": "Captain Jack Sparrow is pursued by an old rival, Captain Salazar, who along with his crew of ghost pirates has escaped from the Devil's Triangle, and is determined to kill every pirate at sea. Jack seeks the Trident of Poseidon, a powerful artifact that grants its possessor total control over the seas, in order to defeat Salazar.",
+      "release_date": "2017-05-24",
+      "genre_ids": [
+        28,
+        12,
+        35,
+        14
+      ],
+      "id": 166426,
+      "original_title": "Pirates of the Caribbean: Dead Men Tell No Tales",
+      "original_language": "en",
+      "title": "Pirates of the Caribbean: Dead Men Tell No Tales",
+      "backdrop_path": "/3DVKG54lqYbdh8RNylXeCf4MBPw.jpg",
+      "popularity": 29.020615,
+      "vote_count": 62,
+      "video": false,
+      "vote_average": 3.8
+    },
+    {
+      "poster_path": "/7RG8JNttOk9RSr8qySdpZRYsl8E.jpg",
+      "adult": false,
+      "overview": "A story about how a new baby's arrival impacts a family, told from the point of view of a delightfully unreliable narrator, a wildly imaginative 7 year old named Tim.",
+      "release_date": "2017-03-23",
+      "genre_ids": [
+        16,
+        35,
+        10751
+      ],
+      "id": 295693,
+      "original_title": "The Boss Baby",
+      "original_language": "en",
+      "title": "The Boss Baby",
+      "backdrop_path": "/frlfy7RFqx5J4jrcMo25PqyasL3.jpg",
+      "popularity": 28.761617,
+      "vote_count": 1135,
+      "video": false,
+      "vote_average": 5.6
+    },
+    {
+      "poster_path": "/5qtySjfuJfOLvVrVXuwNo2BIVPH.jpg",
+      "adult": false,
+      "overview": "When the child Arthur’s father is murdered, Vortigern, Arthur’s uncle, seizes the crown. Robbed of his birthright and with no idea who he truly is, Arthur comes up the hard way in the back alleys of the city. But once he pulls the sword Excalibur from the stone, his life is turned upside down and he is forced to acknowledge his true legacy... whether he likes it or not.",
+      "release_date": "2017-04-27",
+      "genre_ids": [
+        28,
+        18,
+        14
+      ],
+      "id": 274857,
+      "original_title": "King Arthur: Legend of the Sword",
+      "original_language": "en",
+      "title": "King Arthur: Legend of the Sword",
+      "backdrop_path": "/22eFfWlar6MtXO5qG25TkjLphoj.jpg",
+      "popularity": 23.580968,
+      "vote_count": 147,
+      "video": false,
+      "vote_average": 6.3
+    },
+    {
+      "poster_path": "/6cbIDZLfwUTmttXTmNi8Mp3Rnmg.jpg",
+      "adult": false,
+      "overview": "The untold story of Katherine G. Johnson, Dorothy Vaughan and Mary Jackson – brilliant African-American women working at NASA and serving as the brains behind one of the greatest operations in history – the launch of astronaut John Glenn into orbit. The visionary trio crossed all gender and race lines to inspire generations to dream big.",
+      "release_date": "2016-12-10",
+      "genre_ids": [
+        36,
+        18
+      ],
+      "id": 381284,
+      "original_title": "Hidden Figures",
+      "original_language": "en",
+      "title": "Hidden Figures",
+      "backdrop_path": "/kJJ50CR1ktLIHyBHuAXeili85rZ.jpg",
+      "popularity": 12.426983,
+      "vote_count": 1356,
+      "video": false,
+      "vote_average": 7.7
+    },
+    {
+      "poster_path": "/muwaDIv5cLv2Z07CZFWOsL7lYuO.jpg",
+      "adult": false,
+      "overview": "A man without attachments or responsibilities suddenly finds himself with an abandoned baby and leaves for London to try and find the mother. Eight years later after he and his daughter become inseparable Gloria's mother reappears.",
+      "release_date": "2016-12-07",
+      "genre_ids": [
+        18,
+        35
+      ],
+      "id": 382591,
+      "original_title": "Demain tout commence",
+      "original_language": "fr",
+      "title": "Tomorrow Everything Starts",
+      "backdrop_path": "/kiIIOJbV8BCyatCmXqOdJEUXW3u.jpg",
+      "popularity": 11.819052,
+      "vote_count": 392,
+      "video": false,
+      "vote_average": 7.5
+    },
+    {
+      "poster_path": "/mWiqc87iTs1qujxm2Q0NRzGvWSN.jpg",
+      "adult": false,
+      "overview": "In the fourth installment of the fighting franchise, Boyka is shooting for the big leagues when an accidental death in the ring makes him question everything he stands for. When he finds out the wife of the man he accidentally killed is in trouble, Boyka offers to fight in a series of impossible battles to free her from a life of servitude",
+      "release_date": "2017-04-18",
+      "genre_ids": [
+        28
+      ],
+      "id": 348893,
+      "original_title": "Boyka: Undisputed IV",
+      "original_language": "en",
+      "title": "Boyka: Undisputed IV",
+      "backdrop_path": "/5dLLiR8IUT7T0r5HVcvRlItNWTn.jpg",
+      "popularity": 10.816382,
+      "vote_count": 177,
+      "video": false,
+      "vote_average": 5.5
+    },
+    {
+      "poster_path": "/vdHUj9cyRe7fIYdJFMyc7elnawC.jpg",
+      "adult": false,
+      "overview": "A boy attempts to deal with his mother's illness and the bullying of his classmates by escaping to a fantastical world.",
+      "release_date": "2016-10-07",
+      "genre_ids": [
+        18,
+        14
+      ],
+      "id": 258230,
+      "original_title": "A Monster Calls",
+      "original_language": "en",
+      "title": "A Monster Calls",
+      "backdrop_path": "/xVW8REyVqKwxAtUYY07UGlZH43L.jpg",
+      "popularity": 9.520001,
+      "vote_count": 588,
+      "video": false,
+      "vote_average": 6.6
+    },
+    {
+      "poster_path": "/sXf30F2HFpsFPXlNz7jpOySSV9I.jpg",
+      "adult": false,
+      "overview": "When Mahendra, the son of Bahubali, learns about his heritage, he begins to look for answers. His story is juxtaposed with past events that unfolded in the Mahishmati Kingdom.",
+      "release_date": "2017-04-27",
+      "genre_ids": [
+        28,
+        12,
+        10752
+      ],
+      "id": 350312,
+      "original_title": "Baahubali 2: The Conclusion",
+      "original_language": "te",
+      "title": "Baahubali 2: The Conclusion",
+      "backdrop_path": "/yCRYYGldFGVa4cmZVpTVCRQMfjA.jpg",
+      "popularity": 8.601981,
+      "vote_count": 92,
+      "video": false,
+      "vote_average": 6.3
+    },
+    {
+      "poster_path": "/3jcNvhtVQe5Neoffdic39fRactM.jpg",
+      "adult": false,
+      "overview": "A dog goes on quest to discover his purpose in life over the course of several lifetimes with multiple owners.",
+      "release_date": "2017-01-19",
+      "genre_ids": [
+        35,
+        18,
+        10751,
+        14
+      ],
+      "id": 381289,
+      "original_title": "A Dog's Purpose",
+      "original_language": "en",
+      "title": "A Dog's Purpose",
+      "backdrop_path": "/2ddVTlMyATZdvUHE7DPExA2X6xF.jpg",
+      "popularity": 6.865372,
+      "vote_count": 322,
+      "video": false,
+      "vote_average": 6.1
+    },
+    {
+      "poster_path": "/d6AipVF4SDYqxGeivfs3yzsNUs7.jpg",
+      "adult": false,
+      "overview": "Kenny Wells, a modern-day prospector, hustler, and dreamer, is desperate for a lucky break. Left with few options, Wells teams up with an equally luckless geologist to execute a grandiose, last-ditch effort: to find gold deep in the uncharted jungle of Indonesia.",
+      "release_date": "2016-12-22",
+      "genre_ids": [
+        12,
+        80,
+        18
+      ],
+      "id": 334524,
+      "original_title": "Gold",
+      "original_language": "en",
+      "title": "Gold",
+      "backdrop_path": "/ssoXBG2h5afUGD4NwaeAtpSLjCx.jpg",
+      "popularity": 6.30417,
+      "vote_count": 169,
+      "video": false,
+      "vote_average": 6.5
+    },
+    {
+      "poster_path": "/lvGa6v7sR6p6RWCtwv0Dgdr1B8z.jpg",
+      "adult": false,
+      "overview": "When her boyfriend dumps Emily, a spontaneous woman in her 30s, she persuades her ultra-cautious mom to accompany her on a vacation to Ecuador. When these two very different women are trapped on this wild journey, their bond as mother and daughter is tested and strengthened while they attempt to navigate the jungle and escape.",
+      "release_date": "2017-05-11",
+      "genre_ids": [
+        28,
+        35
+      ],
+      "id": 373569,
+      "original_title": "Snatched",
+      "original_language": "en",
+      "title": "Snatched",
+      "backdrop_path": "/6E2TAM2zp2dqgOTLBFHIj8Uu1ip.jpg",
+      "popularity": 6.142933,
+      "vote_count": 16,
+      "video": false,
+      "vote_average": 3.5
+    },
+    {
+      "poster_path": "/hvQgtEMXzQZDCZkkLFPK6cO1FIy.jpg",
+      "adult": false,
+      "overview": "Father and son coroners who receive a mysterious homicide victim with no apparent cause of death. As they attempt to identify the beautiful young \"Jane Doe,\" they discover increasingly bizarre clues that hold the key to her terrifying secrets.",
+      "release_date": "2016-12-21",
+      "genre_ids": [
+        27,
+        9648
+      ],
+      "id": 397243,
+      "original_title": "The Autopsy of Jane Doe",
+      "original_language": "en",
+      "title": "The Autopsy of Jane Doe",
+      "backdrop_path": "/m4LtS8NoVvbl57mej9KBg9QT4fg.jpg",
+      "popularity": 5.93826,
+      "vote_count": 387,
+      "video": false,
+      "vote_average": 6.4
+    },
+    {
+      "poster_path": "/xq1Ugd62d23K2knRUx6xxuALTZB.jpg",
+      "adult": false,
+      "overview": "High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places. Mitsuha wakes up in Taki’s body, and he in hers. This bizarre occurrence continues to happen randomly, and the two must adjust their lives around each other.",
+      "release_date": "2016-08-26",
+      "genre_ids": [
+        16,
+        18,
+        14,
+        10749
+      ],
+      "id": 372058,
+      "original_title": "君の名は。",
+      "original_language": "ja",
+      "title": "Your Name.",
+      "backdrop_path": "/6vkhRvsRvWpmaRVyCXaxTkIEb7j.jpg",
+      "popularity": 5.926513,
+      "vote_count": 502,
+      "video": false,
+      "vote_average": 8.4
+    },
+    {
+      "poster_path": "/ik3ebv7J18fs6cHkmu91oxz7EGt.jpg",
+      "adult": false,
+      "overview": "A true-life drama, centering on British explorer Col. Percival Fawcett, who disappeared while searching for a mysterious city in the Amazon in the 1920s.",
+      "release_date": "2017-03-15",
+      "genre_ids": [
+        28,
+        12,
+        18,
+        36
+      ],
+      "id": 314095,
+      "original_title": "The Lost City of Z",
+      "original_language": "en",
+      "title": "The Lost City of Z",
+      "backdrop_path": "/dAQzSsITJVxyk9ChqPLyBEwtGuC.jpg",
+      "popularity": 5.840205,
+      "vote_count": 109,
+      "video": false,
+      "vote_average": 6
+    },
+    {
+      "poster_path": "/1cg6oZo7C7iqYTZgbfOvscAFiP2.jpg",
+      "adult": false,
+      "overview": "A washed up actor who played Mindhorn, a secret agent with a bionic eye, returns to the Isle of Man, the area where his most famous role was set, to help catch a killer.",
+      "release_date": "2017-05-05",
+      "genre_ids": [
+        35
+      ],
+      "id": 381518,
+      "original_title": "Mindhorn",
+      "original_language": "en",
+      "title": "Mindhorn",
+      "backdrop_path": "/yinWk2gXymXEIChjPUQ50GUN7Mx.jpg",
+      "popularity": 5.823831,
+      "vote_count": 16,
+      "video": false,
+      "vote_average": 6.9
+    }
+  ],
+  "dates": {
+    "maximum": "2017-05-26",
+    "minimum": "2017-04-14"
+  },
+  "total_pages": 35,
+  "total_results": 698,
+  "total_movies": 20
+}
