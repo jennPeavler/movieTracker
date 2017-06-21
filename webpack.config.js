8eb0122db909d0b6b9792dd229aea4c9e213f2f9
Jennifer Peavler <jenn.peavler@gmail.com>
2017-06-20 20:57:10 -0600

    bundle.html

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
    ],
    output: {
      path: path.join(__dirname, 'app'),
-     filename: 'bundle.js',
+     filename: 'bundle.html',
      publicPath: '/app/'
    },
    module: {
